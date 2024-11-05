2bbe55dbc7b82f4a4633b51ae3e8d9cee41d9f14
mikhailabramov9 <mikhailabramov1983@gmail.com>
2024-11-05 22:40:50 +0200

    Initial commit

---
+++ module_1_3/module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Mikhail'
+print (name)
+age = 41
+print (age)
+age = int(age) + 10
+print (age)
+is_student = True
+print (is_student)
---
+++ testmodule/testing.py
@@ -0,0 +1,23 @@
+name=('это моя строка')
+print(name[0])
+print(name[-1])
+print(name[3:6])
+print(name[::-1])
+print(len(name))
+print('это новая строка '+ name[0:])
+
+num1 = 5
+num2 = 3
+sum = (num1 + num2)
+print(sum)
+result = sum + num1 * num2
+print(result)
+
+name = 'Mikhail'
+print (name)
+age = 41
+print (age)
+new_age = 41-10
+print (new_age)
+is_student = (new_age < age)
+print (is_student)
