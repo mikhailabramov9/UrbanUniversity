3e3ab5200e618ac8325d9d0e531cf2d7116c68bb
mikhailabramov9 <mikhailabramov1983@gmail.com>
2024-11-06 18:32:14 +0200

    nome work2

---
+++ module_1_3/hw2.py
@@ -0,0 +1,5 @@
+hw = "12"
+hours = "1.5"
+name = "Python"
+time_per_hw = float(hours) / int(hw)
+print("Курс: ",name,"всего задач: ",hw,", затрачено часов: ", hours, ", среднее время выполнения", time_per_hw, "часа")
