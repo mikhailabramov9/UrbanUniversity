13941dfed7bb87eb75ea4cf9fcf9b0bc0f9020ed
mikhailabramov9 <mikhailabramov1983@gmail.com>
2024-11-10 14:14:52 +0200

    home work 5

---
+++ module_1/module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 25, "Tuple", True, 32, 5, [5,6,1983]
+print(immutable_var)
+#immutable_var[1] = "bubble"
+muttable_list = ["nuts", "bolts", "washers", "springs"]
+print(muttable_list)
+muttable_list [1]= "studs"
+print(muttable_list)
+
