25d83424a4723e6797d742151843c709e717c3b0
mikhailabramov9 <mikhailabramov1983@gmail.com>
2024-12-17 11:50:16 +0200

    add other metods

--- module_5/module_5_1.py
+++ module_5/module_5_1.py
@@ -17,6 +17,42 @@ class House:
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
+    def __eq__(self, other):
+        isinstance(other, int)
+        return self.number_of_floors == other
+
+#Методы __lt__(<), __le__(<=), __gt__(>), __ge__(>=), __ne__(!=)
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+#__add__(self, value) - увеличивает кол-во этажей на переданное значение value, возвращает сам объект self.
+    def __add__(self, value):
+        self.number_of_floors = self.number_of_floors + value
+        return self
+
+#__radd__(self, value), __iadd__(self, value) - работают так же как и __add__ (возвращают результат его вызова).
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+    def __radd__(self, value):
+        self.number_of_floors = value + self.number_of_floors
+        return self
+
+
+
+
 # h1 = House('ЖК Горский', 18)
 # h2 = House('Домик в деревне', 2)
 # h3 = House('ЖК Эльбрус', 30)
@@ -28,13 +64,32 @@ class House:
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-
 #__str__
+# print(h1)
+# print(h2)
+
+# # __len__
+# print(len(h1))
+# print(len(h2))
+
+
 print(h1)
 print(h2)
-# print(h3)
 
-# __len__
-print(len(h1))
-print(len(h2))
-# print(len(h3))
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
